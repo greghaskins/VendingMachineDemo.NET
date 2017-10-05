3251c6f71460f97eaed979bffebc2300e0a0a3e9
Gregory Haskins <greg@greghaskins.com>
2017-10-05 17:02:18 -0400

    A nickel is worth five cents

---
+++ Vending/CoinAppraiser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vending
+{
+    public class CoinAppraiser
+    {
+        public int Appraise(Coin coin)
+        {
+            return 5;
+        }
+    }
+}
---
+++ VendingTests/CoinAppraiserTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Vending;
+
+namespace VendingTests
+{
+    [TestFixture]
+    class CoinAppraiserTest
+    {
+        [Test]
+        public void ANickelIsWorthFiveCents()
+        {
+            Assert.AreEqual(5, new CoinAppraiser().Appraise(Coin.Nickel));
+        }
+    }
+}
